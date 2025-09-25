a94b04c85393f7a9ab7730db0851daafccbec1ed
paswanashish189-glitch <paswanashish189@gmail.com>
2025-09-25 20:32:58 +0530

    Add files via upload

---
+++ 1assignment.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+int m;
+printf("enter m:");
+scanf ("%d,&m");
+
+if(m>0) {
+ printf("%d n is 1",m);
+
+
+} else if(m==0){
+
+printf("%d n isis 0",m);
+
+} else if (m<0){
+
+      printf("%d n is -1",m);
+
+
+}
+
+return 0;
+
+}
+
+
+
