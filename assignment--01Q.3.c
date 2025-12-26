f288144e50eed4cbfc78cc3b0b0658b7ed328ef1
paswanashish189-glitch <paswanashish189@gmail.com>
2025-12-26 10:44:57 +0530

    Add files via upload

---
+++ assignment--01Q.3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+float km;
+printf("Enter the distance of two cities in kilometer ");
+
+scanf("%f",&km);
+printf("your enter distance in feet  is %f\n",km*3280.84);
+printf("your enter distance in inch is %f\n",km*39370.1);
+printf("your enter distance in centimeter is %f",km*100000);
+
+
+
+
+
+
+
+
+}
