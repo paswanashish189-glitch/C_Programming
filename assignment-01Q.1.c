bf6c62809ac9333c78eb43a8c2dee040c40bb7be
paswanashish189-glitch <paswanashish189@gmail.com>
2025-12-26 10:37:36 +0530

    Add files via upload

---
+++ assignment-01Q.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+float radius;
+float pi=3.14159;
+printf("Enter the radius of the circle\n");
+printf("Radius:");
+scanf("%f",&radius);
+
+printf("Area of the circle= %.2f\n",pi*radius*radius);
+printf("perimeter of the circle= %.2f",2*pi*radius);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
